2f6e3174f6d0cf6a993fd4ebc629d52b144fde05
Siddharth Singh <singhsiddharth656@gmail.com>
2022-09-03 00:50:08 +0530

    leaflet map

--- script.js
+++ script.js
@@ -17,7 +17,17 @@ navigator.geolocation.getCurrentPosition(function(position){
     const {latitude, longitude} = position.coords;
     console.log(latitude, longitude);
     console.log(`https://www.google.co.in/maps/@${latitude},${longitude}`);
-}, function(){
-    alert('Could not get your position')
-});
-}
+    const coords = [latitude, longitude]
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(coords).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    }, function(){
+        alert('Could not get your position')
+    });
+    }
